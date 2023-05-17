812a70f28b557fc1e345ebea9fc0a39be8285c5a
Petyo Ivanov <underlog@gmail.com>
2023-05-17 20:13:36 +0300

    Repro

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,11 @@
+import { Virtuoso } from 'react-virtuoso'
+
+const itemContent = (index: number) => <div style={{ height: index % 2 ? 30 : 20, background: 'white' }}>Item {index}</div>
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  return <Virtuoso totalCount={100} itemContent={itemContent} initialTopMostItemIndex={99} style={{ height: 300 }} atBottomStateChange={(atBottom) => console.log({atBottom})}
+    startReached={() => console.log('start reached')}
+  />
+
 }
